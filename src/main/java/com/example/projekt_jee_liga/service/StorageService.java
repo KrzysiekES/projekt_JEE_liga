b5c30481a178a4941bfe230886562d0a81f723dc
Krzysiek <krzysiekes.polgmail.com>
2015-11-14 18:57:15 +0100

    zmiany w storage Service

--- src/main/java/com/example/projekt_jee_liga/service/StorageService.java
+++ src/main/java/com/example/projekt_jee_liga/service/StorageService.java
@@ -3,20 +3,33 @@ package main.java.com.example.projekt_jee_liga.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.com.example.projekt_jee_liga.domain.Club;
 import main.java.com.example.projekt_jee_liga.domain.Player;
 
 public class StorageService {
 	int usun;
 	
-	private List<Player> db = new ArrayList<Player>();
+	private List<Player> players = new ArrayList<Player>();
+	private List<Club> clubs = new ArrayList<Club>();
+	
 	
 	public void add(Player player){
 		Player newPlayer = new Player (player.getPlayerId(), player.getClubId(), player.getFirstName(), player.getLastName(), player.getPosition(), player.getNumber());
-		db.add(newPlayer);
+		players.add(newPlayer);
 	}
 	
 	public List<Player> getAllPlayers(){
-		return db;
+		return players;
+	}
+	
+	public void add(Club club){
+		Club newClub = new Club (club.getClubId(), club.getClubName(), club.getClubCity(), club.getClubMenager());
+		
+		clubs.add(newClub);
+	}
+	
+	public List<Club> getAllClubs(){
+		return clubs;
 	}
 
 }
